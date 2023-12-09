5b13cd84166108b85cfc3dd71f7bc3adfb874be5
Sreeraj-SR <sreerajsree9961@gmail.com>
2023-12-09 11:07:12 +0530

    Drum Kit task Completed

--- DOM/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/index.js
@@ -12,7 +12,7 @@ let  buttons = document.querySelectorAll(".drum"); // Taking the all buttons wit
 
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(){
-            console.log(this)
+            buttonAnimation(this.innerHTML)
             switch (this.innerHTML){
                 case "w":   var tom1 = new Audio("sounds/tom-1.mp3")
                             tom1.play();
@@ -38,4 +38,40 @@ let  buttons = document.querySelectorAll(".drum"); // Taking the all buttons wit
                 default: console.log(this.innerHTML)
                 }   
     })
+}
+
+document.addEventListener("keydown", (event) => {
+    buttonAnimation(event.key.toLowerCase())
+    switch (event.key.toLowerCase()){
+        case "w":   var tom1 = new Audio("sounds/tom-1.mp3")
+                    tom1.play();
+                    break; 
+        case "a":   var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+        case "s":   var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+        case "d":   var tom4 = new Audio("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+        case "j":   var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                    break;
+        case "k":   var crash = new Audio("sounds/crash.mp3");
+                    crash.play();
+                    break;
+        case "l":   var kick = new Audio("sounds/kick-bass.mp3")
+                    kick.play();
+                    break;
+        default: console.log(this.innerHTML)
+        }   
+})
+
+buttonAnimation = (currentKey) => {
+    var activeButton = document.querySelector(`.${currentKey}`)
+    activeButton.classList.add("pressed")
+    setTimeout(() => {
+        activeButton.classList.remove("pressed")
+    }, 10)
 }
